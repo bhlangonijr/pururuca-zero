613bff920e5d9cc7329237880e73dab9166f2964
Ben-Hur Carlos Vieira Langoni Junior <bhlangonijr@gmail.com>
2018-11-27 01:01:55 +0100

    tweaking

--- src/main/java/com/github/bhlangonijr/chesslib/eval/StatEval.kt
+++ src/main/java/com/github/bhlangonijr/chesslib/eval/StatEval.kt
@@ -1,14 +1,21 @@
 package com.github.bhlangonijr.chesslib.eval
 
-import com.github.bhlangonijr.chesslib.Board
-import com.github.bhlangonijr.chesslib.Piece
-import com.github.bhlangonijr.chesslib.Square
+import com.github.bhlangonijr.chesslib.*
 import kotlin.math.abs
 import kotlin.math.max
 
 class StatEval {
 
     private val pieces = Piece.values().filter { it != Piece.NONE }
+    private val distances = Rank.values().size - 1
+    private val pieceTypeValues = mapOf(
+            PieceType.NONE to 0.0,
+            PieceType.PAWN to 1.0,
+            PieceType.KNIGHT to 3.0,
+            PieceType.BISHOP to 7.0,
+            PieceType.ROOK to 19.0,
+            PieceType.QUEEN to 53.0,
+            PieceType.KING to 517.0)
 
     fun predict(board: Board): Double {
 
@@ -18,7 +25,8 @@ class StatEval {
 
     fun extractFeatures(board: Board): Array<Double> {
 
-        val result = DoubleArray(pieces.size * pieces.size) { -1.0 }
+        val distanceSide = DoubleArray(pieces.size * distances) { -1.0 }
+        val distanceOther = DoubleArray(pieces.size * distances) { -1.0 }
 
         val pieceLocation = pieces
                 .filter { board.getBitboard(it) > 0 }
@@ -28,16 +36,20 @@ class StatEval {
             squares.forEach { sq1 ->
                 pieceLocation.entries.forEach { (p2, squares) ->
                     squares.forEach { sq2 ->
-                        result[p1.ordinal * pieces.size + p2.ordinal] += distance(sq1, sq2)
+                        if (p2.pieceSide == p1.pieceSide) {
+                            distanceSide[p1.ordinal * distances + distance(sq1, sq2)] += pieceTypeValues[p2.pieceType]!!
+                        } else {
+                            distanceOther[p1.ordinal * distances + distance(sq1, sq2)] += pieceTypeValues[p2.pieceType]!!
+                        }
                     }
                 }
             }
         }
-        return result.toTypedArray()
+        return distanceSide.toTypedArray() + distanceOther.toTypedArray()
     }
 
     private fun distance(sq1: Square, sq2: Square) =
-            max(abs(sq1.file.ordinal - sq2.file.ordinal), abs(sq1.rank.ordinal - sq2.rank.ordinal)).toDouble()
+            max(abs(sq1.file.ordinal - sq2.file.ordinal), abs(sq1.rank.ordinal - sq2.rank.ordinal))
 
     private fun bitCount(bb: Long) = java.lang.Long.bitCount(bb)
 
